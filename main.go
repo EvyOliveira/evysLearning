d7ccc7a2f8488930942ec61048c2ed98f0ae48b1
evyoliveira <cristinioliveira.eo@gmail.com>
2024-04-11 20:43:08 -0300

    Add exercises routes and functions

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type exercise struct {
-	ID            uint   `json:"id"`
+	ID            string `json:"id"`
 	Question      string `json:"question"`
 	Answers       string `json:"answers"`
 	CorrectAnswer string `json:"correct_answer"`
 	Subject       string `json:"subject"`
-	//Classes       class  `json:"classes"`
+	Classes       class  `json:"classes"`
 }
 
 type class struct {
-	ID     uint   `json:"id"`
+	ID     string `json:"id"`
 	Title  string `json:"title"`
 	Resume string `json:"resume"`
 	Text   string `json:"text"`
@@ -26,54 +29,89 @@ type class struct {
 }
 
 type course struct {
-	ID          uint `json:"id"`
+	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
-var exercises = []exercise{
-	{ID: 1, Question: "2", Answers: "D", CorrectAnswer: "E", Subject: "Math"},
-	{ID: 20, Question: "2", Answers: "A", CorrectAnswer: "C", Subject: "Biology"},
-}
-
+var exercises []exercise
 
 func main() {
-	http.HandleFunc("/", getAll)
-	fmt.Println("api is on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	route := mux.NewRouter()
 
-	resp, err := http.Get("http://localhost:8080/")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	route.HandleFunc("/exercises", getExercises).Methods("GET")
+	route.HandleFunc("/exercises/{id}", getExercise).Methods("GET")
+	route.HandleFunc("/exercises", createExercise).Methods("POST")
+	route.HandleFunc("/exercises/", updateExercise).Methods("PUT")
+	route.HandleFunc("/exercises/", deleteExercise).Methods("DELETE")
 
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Not success", resp.StatusCode)
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-
-	var response []exercise
-
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		fmt.Println("erro ao recuperar dados", err.Error())
-		return
-	}
-
-	fmt.Println(response)
+	fmt.Println("starting server at port:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func getAll(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-	}
-
+func getExercises(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	err := json.NewEncoder(w).Encode(exercises)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func getExercise(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	params := mux.Vars(r)
+
+	for _, item := range exercises {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createExercise(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	var exercise exercise
+
+	_ = json.NewDecoder(r.Body).Decode(&exercise)
+	exercise.ID = strconv.Itoa(rand.Intn(100000000))
+
+	exercises = append(exercises, exercise)
+	json.NewEncoder(w).Encode(exercise)
+}
+
+func updateExercise(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	params := mux.Vars(r)
+
+	for index, item := range exercises {
+		if item.ID == params["id"] {
+			exercises = append(exercises[:index], exercises[index+1:]...)
+
+			var exercise exercise
+			_ = json.NewDecoder(r.Body).Decode(&exercise)
+			exercise.ID = params["id"]
+			exercises = append(exercises, exercise)
+			json.NewEncoder(w).Encode(exercise)
+			return
+		}
+	}
+}
+
+func deleteExercise(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	params := mux.Vars(r)
+
+	for index, item := range exercises {
+		if item.ID == params["id"] {
+			exercises = append(exercises[:index], exercises[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(exercises)
 }
